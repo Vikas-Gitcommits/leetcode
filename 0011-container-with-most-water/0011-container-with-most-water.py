b3660ec6741dc1aa2f1207198ecae049380b8cb5
Vikas-Gitcommits <vikasshettigar11@gmail.com>
2026-08-01 11:04:23 +0530

    Time: 57 ms (60.77%), Space: 29.6 MB (67.29%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,7 +3,8 @@ class Solution:
         l=0
         r=len(h)-1
         maarea=0
-        while(l<r):
+       
+        while l<r:
             if h[l]<=h[r]:
                 area=h[l]*(r-l)
                 l+=1
@@ -12,4 +13,3 @@ class Solution:
                 r-=1
             maarea=max(maarea,area)
         return maarea
-        
