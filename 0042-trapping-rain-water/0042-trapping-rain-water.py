7b9217030aef6558e464bda6efcf7554767aacba
Vikas-Gitcommits <vikasshettigar11@gmail.com>
2026-08-01 10:11:05 +0530

    Time: 12 ms (31.53%), Space: 21 MB (48.14%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,17 @@
+class Solution:
+    def trap(self, h: List[int]) -> int:
+        l=0
+        r=len(h)-1
+        lb=h[l]
+        rb=h[r]
+        w=0
+        while l<r:
+            if lb<=rb:
+                l+=1
+                lb=max(lb,h[l])
+                w+=(lb-h[l])
+            else:
+                r-=1
+                rb=max(rb,h[r])
+                w+=(rb-h[r])
+        return w
